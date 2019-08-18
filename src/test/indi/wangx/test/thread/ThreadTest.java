027ae66aa9d896a5cc1d1b4803f0e9e248aa2a30
wangx <2763041366@qq.com>
2019-08-18 16:34:01 +0800

    another commit

--- src/test/indi/wangx/test/thread/ThreadTest.java
+++ src/test/indi/wangx/test/thread/ThreadTest.java
@@ -1,12 +1,12 @@
 package indi.wangx.test.thread;
 /**
- * java Thread test
+ * java Thread test 
  * @author 27630
  * @version 2019-08-18
  */
 public class ThreadTest {
 
 	public static void main(String[] args) {
-		
+		Thread t = new Thread();
 	}
 }
