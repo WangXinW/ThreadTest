37fe794fa950f2b4840b061ac96a5d91e6b790e4
wangx <2763041366@qq.com>
2019-08-18 17:24:35 +0800

    1

--- src/test/indi/wangx/test/thread/ThreadTest.java
+++ src/test/indi/wangx/test/thread/ThreadTest.java
@@ -1,10 +1,6 @@
 package indi.wangx.test.thread;
 /**
-<<<<<<< HEAD
  * java Thread test   
-=======
- * java Thread test  
->>>>>>> branch 'master' of https://github.com/WangXinW/ThreadTest.git
  * @author 27630
  * @version 2019-08-18
  */
