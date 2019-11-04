473b8740c049ee10fc5ce641a72e02c0422dd94c
wangx <2763041366@qq.com>
2019-11-04 22:45:31 +0800

    gitTest

--- src/test/indi/wangx/test/git1/TestGit1.java
+++ src/test/indi/wangx/test/git1/TestGit1.java
@@ -12,4 +12,12 @@ public class TestGit1 {
     }
     
     private String testGit1;
+
+    public String getTestGit1() {
+        return testGit1;
+    }
+
+    public void setTestGit1(String testGit1) {
+        this.testGit1 = testGit1;
+    }
 }
